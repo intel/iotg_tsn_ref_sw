07f86edffa561cc01055055ee29d9d7476e0a9a6
Noor Azura Ahmad Tarmizi <noor.azura.ahmad.tarmizi@intel.com>
2021-04-05 00:56:19 +0800

    shell/vs1x: Added TEMP_DIR config for output folder customization
    Using TEMP_DIR, output directory can now be dynamicaly changed using
    the vs1x.config folder.
    
    NOTE: For ADL, output is using /dev/shm due to a current ADL system bug.
    A high volume access to any /tmp file is causing the system to hang.
    
    Signed-off-by: Noor Azura Ahmad Tarmizi <noor.azura.ahmad.tarmizi@intel.com>

--- shell/vs1b.sh
+++ shell/vs1b.sh
@@ -40,7 +40,8 @@ if [ -z $1 ]; then
 elif [[ -z $INTERVAL || -z $XDP_INTERVAL ||
        -z $EARLY_OFFSET || -z $XDP_EARLY_OFFSET ||
        -z $TXTIME_OFFSET || -z $NUMPKTS || -z $SIZE ||
-       -z $RX_PKT_Q || -z $RX_XDP_Q || -z $XDP_MODE ]]; then
+       -z $RX_PKT_Q || -z $RX_XDP_Q || -z $XDP_MODE ||
+       -z $TEMP_DIR ]]; then
         echo "Source config file first"; exit
 fi
 
@@ -49,8 +50,8 @@ pkill txrx-tsn
 pkill iperf3
 
 # Improve performance/consistency by logging to tmpfs (system memory)
-ln -sfv /tmp/afpkt-rxtstamps.txt .
-ln -sfv /tmp/afxdp-rxtstamps.txt .
+ln -sfv $TEMP_DIR/afpkt-rxtstamps.txt .
+ln -sfv $TEMP_DIR/afxdp-rxtstamps.txt .
 
 echo 0 > afpkt-rxtstamps.txt
 echo 0 > afxdp-rxtstamps.txt
