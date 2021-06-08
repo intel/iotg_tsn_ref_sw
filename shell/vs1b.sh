876dde0fe7f3b5879480bee75d584fabbbf27194
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2021-06-08 19:37:29 +0800

    I225: Re-init back the setup for the next run
    This is to ensure that socket is tear down completely
    by initializing the vs1 setup again once finish run.
    
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>

--- shell/vs1b.sh
+++ shell/vs1b.sh
@@ -135,6 +135,8 @@ else
     if [[ $PLAT == i225* ]]; then
         echo -e "Resetting the link for i225"
         setup_link_down_up $IFACE
+        sleep 3
+        sh $DIR/setup-vs1a.sh $IFACE
     fi
 fi
 
