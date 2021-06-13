1dd721018b7f22d12c90c56606e5b804d1c0719a
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2021-06-13 16:29:06 +0800

    I225: Fix using wrong setup file
    This is to fix using wrong setup file for Intel Discrete I225 after af_xdp operation
    to ensure that previous socket been tear down completely and reinitialize the setup
    again for kernel 5.4 only. No further debugging if any issue happen on
    kernel 5.4 as marching forward to kernel 5.10.
    
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>

--- shell/vs1a.sh
+++ shell/vs1a.sh
@@ -138,11 +138,12 @@ sleep $XDP_SLEEP_SEC
 pkill iperf3
 pkill txrx-tsn
 
-if [[ $PLAT == i225* ]]; then
-        echo -e "Resetting the link for i225"
-        setup_link_down_up $IFACE
-        sleep 3
-        sh $DIR/setup-vs1a.sh $IFACE
+if [[ $PLAT == i225* && "$KERNEL_VER" != "5.10" ]]; then
+	# To ensure the AF_XDP socket tear down is complete in i225, interface is reset.
+	echo "Re run vs1a setup for af_xdp operation"
+	setup_link_down_up $IFACE
+	sleep 2
+	sh $DIR/setup-vs1a.sh $IFACE
 fi
 
 echo "Done!"
